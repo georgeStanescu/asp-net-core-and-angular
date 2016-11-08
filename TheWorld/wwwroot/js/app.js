899138586ca21023123135e5069b7e5385761f52
gstanescu <georgestanescu90@gmail.com>
2016-11-08 12:04:04 +0200

    introducing javascript file

---
+++ TheWorld/wwwroot/js/app.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var el = document.getElementById("username");
+    el.innerHTML = "Someone";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style = "background-color: #888888;";
+    };
+
+    main.onmouseleave = function () {
+        main.style = "background-color: none;";
+    }
+})();
