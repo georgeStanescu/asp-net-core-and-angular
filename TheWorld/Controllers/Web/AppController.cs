2feb0751aa846ce62017a7779e4d3c0ddf37fd46
gstanescu <georgestanescu90@gmail.com>
2016-11-08 16:24:33 +0200

    contact action: more validations, added config file and read the values from there

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,13 @@ namespace TheWorld.Controllers.Web
     public class AppController : Controller
     {
         private IMailService _mailService;
+        private IConfigurationRoot _config;
 
-        public AppController(IMailService mailService)
+        public AppController(IMailService mailService, IConfigurationRoot config)
         {
             _mailService = mailService;
+
+            _config = config;
         }
 
         public IActionResult Index()
@@ -31,7 +35,19 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel viewModel)
         {
-            _mailService.SendMail("me@me.com", viewModel.Email, "From TheWorld", viewModel.Message);
+            if (viewModel.Email.EndsWith("aol.com"))
+            {
+                ModelState.AddModelError(nameof(viewModel.Email), "We don't support AOL addresses");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettigs:ToAddress"], viewModel.Email, "From TheWorld", viewModel.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent!";
+            }
 
             return View();
         }
